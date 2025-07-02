876e00211b5160111683fef6484f1572c82f6b5b
Rory Bowcott <rorybowcott@gmail.com>
2025-07-02 08:56:13 +0100

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// main.js
+import './style.css';
+
+// Optional: load MDC Web components if you use any interactive ones
+import 'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js';
+
+window.mdc?.autoInit?.();
